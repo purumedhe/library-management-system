94e0b685a3658ff9a5547254f7b554d05d681eb8
purumedhe <161282520+purumedhe@users.noreply.github.com>
2026-07-01 08:47:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function login(){
 
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username=="" || password==""){
+        alert("Please fill all fields");
+        return;
+    }
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    window.location.href="dashboard.html";
+}
