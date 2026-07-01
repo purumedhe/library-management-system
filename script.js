2ea42bd5d5658891442f8257642a4c8e283ffae7
purumedhe <161282520+purumedhe@users.noreply.github.com>
2026-07-01 08:49:41 +0530

    Add book management functions and user welcome

--- script.js
+++ script.js
@@ -13,3 +13,62 @@ function login(){
 
     window.location.href="dashboard.html";
 }
+if(window.location.pathname.includes("dashboard.html")){
+
+    let user = localStorage.getItem("username");
+    document.getElementById("welcome").innerHTML = "Welcome, " + user;
+
+    showBooks();
+}
+
+function addBook(){
+
+    let book = document.getElementById("book").value;
+
+    if(book==""){
+        alert("Enter Book Name");
+        return;
+    }
+
+    let books = JSON.parse(localStorage.getItem("books")) || [];
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+
+    document.getElementById("book").value="";
+
+    showBooks();
+}
+
+function showBooks(){
+
+    let books = JSON.parse(localStorage.getItem("books")) || [];
+
+    let list="";
+
+    for(let i=0;i<books.length;i++){
+
+        list += "<li>"+books[i]+" <button onclick='deleteBook("+i+")'>Delete</button></li>";
+
+    }
+
+    document.getElementById("bookList").innerHTML=list;
+}
+
+function deleteBook(index){
+
+    let books = JSON.parse(localStorage.getItem("books")) || [];
+
+    books.splice(index,1);
+
+    localStorage.setItem("books",JSON.stringify(books));
+
+    showBooks();
+}
+
+function logout(){
+
+    window.location.href="index.html";
+
+}
